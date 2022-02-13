98844df9446c62b7f70aba9d2a8dfc173eebe700
Paulo Muniz <94541244+paullomuniz@users.noreply.github.com>
2022-02-13 14:53:12 -0300

    Add files via upload

---
+++ relogio/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace relogio
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbHoras = new System.Windows.Forms.Label();
+            this.lbMinutos = new System.Windows.Forms.Label();
+            this.lbSegundos = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbData = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbHoras
+            // 
+            this.lbHoras.AutoSize = true;
+            this.lbHoras.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbHoras.Location = new System.Drawing.Point(21, 92);
+            this.lbHoras.Name = "lbHoras";
+            this.lbHoras.Size = new System.Drawing.Size(151, 107);
+            this.lbHoras.TabIndex = 0;
+            this.lbHoras.Text = "18";
+            // 
+            // lbMinutos
+            // 
+            this.lbMinutos.AutoSize = true;
+            this.lbMinutos.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbMinutos.Location = new System.Drawing.Point(154, 120);
+            this.lbMinutos.Name = "lbMinutos";
+            this.lbMinutos.Size = new System.Drawing.Size(102, 72);
+            this.lbMinutos.TabIndex = 1;
+            this.lbMinutos.Text = "45";
+            // 
+            // lbSegundos
+            // 
+            this.lbSegundos.AutoSize = true;
+            this.lbSegundos.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbSegundos.Location = new System.Drawing.Point(251, 149);
+            this.lbSegundos.Name = "lbSegundos";
+            this.lbSegundos.Size = new System.Drawing.Size(109, 36);
+            this.lbSegundos.TabIndex = 2;
+            this.lbSegundos.Text = "57 sec";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::relogio.Properties.Resources.icon_lua;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(317, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbData
+            // 
+            this.lbData.AutoSize = true;
+            this.lbData.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbData.Location = new System.Drawing.Point(59, 253);
+            this.lbData.Name = "lbData";
+            this.lbData.Size = new System.Drawing.Size(290, 32);
+            this.lbData.TabIndex = 4;
+            this.lbData.Text = "Domingo, 13 Fevereiro";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 309);
+            this.Controls.Add(this.lbData);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbSegundos);
+            this.Controls.Add(this.lbMinutos);
+            this.Controls.Add(this.lbHoras);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbHoras;
+        private System.Windows.Forms.Label lbMinutos;
+        private System.Windows.Forms.Label lbSegundos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbData;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ relogio/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace relogio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Color corFundo = Color.FromArgb(25, 47, 51);
+            this.BackColor = corFundo;
+
+            lbHoras.ForeColor = Color.White;
+            lbMinutos.ForeColor = Color.White;
+            lbSegundos.ForeColor = Color.White;
+            lbData.ForeColor = Color.White;
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbHoras.Text = DateTime.Now.ToString("HH");
+            lbMinutos.Text = DateTime.Now.ToString("mm");
+            lbSegundos.Text = DateTime.Now.ToString("ss") + " sec";
+
+            int dia = Convert.ToInt32(DateTime.Now.ToString("dd"));
+            int mes = Convert.ToInt32(DateTime.Now.ToString("MM"));
+            int ano = Convert.ToInt32(DateTime.Now.ToString("yyyy"));
+
+            DateTime dateValue = new DateTime(ano, mes, dia);
+            string strDiaSemana = dateValue.ToString("ddddddddddd");
+
+            lbData.Text = strDiaSemana + ", " + dia.ToString() + " " + DateTime.Now.ToString("MMMM");
+        }
+    }
+}
